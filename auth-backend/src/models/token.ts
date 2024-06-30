4443e3f6a986d788d0f740be063bfca1d5c8442a
Nikos Astrinakis <nikos.astrin@gmail.com>
2024-06-30 18:04:29 +0300

    Add a token model

---
+++ auth-backend/src/models/token.ts
@@ -0,0 +1,39 @@
+import mongoose, { Document, Schema } from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
+import User from './user';
+
+export interface TokenModel extends Document {
+  user: Schema.Types.ObjectId;
+  token: string;
+  expiresAt: Date;
+  createdAt?: Date;
+  revokedAt?: Date;
+  replacedBy?: string;
+}
+
+const tokenSchema = new Schema<TokenModel>({
+  user: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: User,
+  },
+  token: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  expiresAt: {
+    type: Date,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  revokedAt: Date,
+  replacedBy: String,
+});
+
+tokenSchema.plugin(uniqueValidator);
+
+export default mongoose.model<TokenModel>('token', tokenSchema);
