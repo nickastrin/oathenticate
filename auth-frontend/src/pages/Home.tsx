ca21f7c05582d8a9588ffea54d2e17e1a32f7a62
nastrinakis <nastrinakis@softone.gr>
2024-08-06 15:36:33 +0300

    Create a base version of the home page

--- auth-frontend/src/pages/Home.tsx
+++ auth-frontend/src/pages/Home.tsx
@@ -1,26 +1,27 @@
-import { Card } from '@/components/generics/Card';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 
 export function Home(): React.JSX.Element {
-  const navigate = useNavigate();
-
   return (
     <>
-      <div className="flex flex-row gap-6 size-full">
-        <div className="flex flex-col h-full">
-          <Card onClick={() => navigate('/login')}>
-            <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-              ipsam recusandae velit facilis asperiores vitae reiciendis labore
-              nemo ratione esse nostrum cupiditate, laborum expedita dolores ea
-              enim temporibus quas ex?
-            </span>
-          </Card>
+      <div className="flex flex-col size-full p-8 gap-12 md:gap-16 justify-center text-start">
+        <div className = 'flex flex-col gap-2'>
+          <span className = 'ms-2 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-light text-zinc-400'>
+            Welcome to
+          </span>
+          <span className = 'text-7xl sm:text-8xl md:text-9xl lg:text-9xl font-extrabold text-teal-200'>
+            OATH.
+          </span>
         </div>
 
-        <div className="flex flex-col grow">
-          <Card onClick={() => navigate('/login')}>hello</Card>
+        <div className = 'font-light text-md md:text-2xl max-w-md text-zinc-400'>
+          <span> Add </span>
+          <span className = 'text-pink-500 font-bold leading-10'> unbroken </span>
+          <span> security to your app now. The safety of your users is one click away. </span>
         </div>
+
+        <button className = 'bg-teal-200 bg-opacity-30 text-teal-100 border-teal-100 rounded-full py-3 px-8 w-fit'>
+          Get started
+        </button>
       </div>
     </>
   );
