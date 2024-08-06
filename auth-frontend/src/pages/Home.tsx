e72205c073c9b3084437e86feb1490e516086009
nastrinakis <nastrinakis@softone.gr>
2024-08-06 16:12:42 +0300

    Enhance home page

--- auth-frontend/src/pages/Home.tsx
+++ auth-frontend/src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import svg from '@/assets/laptop.svg';
 
 export function Home(): React.JSX.Element {
   return (
-    <>
-      <div className="flex flex-col size-full p-8 gap-12 md:gap-16 justify-center text-start">
+    <div className = 'flex flex-row size-full p-8 container mx-auto justify-between gap-4'>
+      <div className="flex flex-col gap-12 md:gap-16 justify-center text-start">
         <div className = 'flex flex-col gap-2'>
-          <span className = 'ms-2 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-light text-zinc-400'>
+          <span className = 'ms-2 text-4xl sm:text-5xl md:text-6xl lg:text-6xl xl:text-7xl font-light text-zinc-400'>
             Welcome to
           </span>
           <span className = 'text-7xl sm:text-8xl md:text-9xl lg:text-9xl font-extrabold text-teal-200'>
@@ -19,10 +20,15 @@ export function Home(): React.JSX.Element {
           <span> security to your app now. The safety of your users is one click away. </span>
         </div>
 
-        <button className = 'bg-teal-200 bg-opacity-30 text-teal-100 border-teal-100 rounded-full py-3 px-8 w-fit'>
+        <button className = 'bg-teal-200/30 text-teal-100 rounded-full py-3 px-8 w-fit outline outline-1 hover:outline-2 hover:outline-teal-200 '>
           Get started
         </button>
       </div>
-    </>
+      <div className = 'group relative hidden lg:flex lg:flex-col justify-center 2xl:w-1/2'>
+        <img src = {svg as string} className = 'z-20' alt = {'Laptop'}/>
+        <div className = 'absolute top-50 left-0 w-full aspect-square border-2 border-teal-200 bg-teal-200/30 rounded-full -z-10 transition-all duration-300 group-hover:shadow-2xl group-hover:shadow-pink-400'>
+        </div>
+      </div>
+    </div>
   );
 }
