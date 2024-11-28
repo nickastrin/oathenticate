18c85c0a4ea5d3b4ad581683371170e3cbdfd908
Nikos Astrinakis <nikos.astrin@gmail.com>
2024-11-28 23:46:46 +0200

    Create the authentication controller

---
+++ auth-backend/src/controllers/auth-controller.ts
@@ -0,0 +1,38 @@
+import { UserModel } from "@interfaces/interfaces";
+import { AuthenticationService } from "@services/services";
+import { setTokenCookie } from "@utils/utils";
+import { Request, Response, NextFunction } from "express";
+
+export class AuthenticationController {
+  private authService: AuthenticationService;
+
+  constructor(authService: AuthenticationService) {
+    this.authService = authService;
+  }
+
+  public async register(req: Request, res: Response, next: NextFunction) {
+    const data: UserModel = req.body;
+
+    await this.authService
+      .createUser(data)
+      .then(() => {
+        res.json({ message: "User created successfully" });
+      })
+      .catch(next);
+  }
+
+  public async authenticate(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body;
+
+    await this.authService
+      .authenticate(email, password)
+      .then(({ accessToken, refreshToken }) => {
+        setTokenCookie(res, refreshToken ?? "");
+        res.json({
+          message: "User logged in successfully",
+          token: accessToken,
+        });
+      })
+      .catch(next);
+  }
+}
