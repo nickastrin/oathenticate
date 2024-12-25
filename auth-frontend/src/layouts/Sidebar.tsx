7caaa8872999432ed7a8e56c3a7d593c2fca7167
nastrinakis <nastrinakis@softone.gr>
2024-12-25 11:08:46 +0200

    Add some styling to the sidebar

--- auth-frontend/src/layouts/Sidebar.tsx
+++ auth-frontend/src/layouts/Sidebar.tsx
@@ -2,8 +2,13 @@ import { NavigationButton } from "@/components/navigation/NavigationButton";
 
 export function Sidebar() {
   return (
-    <div className="flex flex-col justify-between items-center py-4 px-2 bg-zinc-800 rounded-r-xl">
-      <div className="flex flex-col gap-4 items-center">
+    <div className="py-8 px-2 bg-dark rounded-r-xl flex flex-col">
+      <div className="grid gap-8 place-items-center mb-auto">
+        <div className="font-montserrat font-extrabold">
+          <span className="tracking-widest">OATH</span>
+          <span className="text-primary-light">.</span>
+        </div>
+
         <NavigationButton icon="home" label="Home" onClick={() => {}} />
         <NavigationButton icon="settings" label="Settings" onClick={() => {}} />
         <NavigationButton icon="groups" label="About" onClick={() => {}} />
