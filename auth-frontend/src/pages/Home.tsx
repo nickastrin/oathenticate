26ab49cbf9b95c1c80f48c3b5cd5286d18e7d098
Nikos Astrinakis <65467988+nickastrin@users.noreply.github.com>
2024-12-26 23:16:33 +0200

    Feature/home page (#12)
    ## Description
    Recreate home screen.
    
    ### List of changes
    - Visual remake of the home page to match Figma designs.

--- auth-frontend/src/pages/Home.tsx
+++ auth-frontend/src/pages/Home.tsx
@@ -1,55 +1,49 @@
-import svg from "@/assets/laptop.svg";
-import github from "@/assets/social/github.svg";
-import linkedIn from "@/assets/social/linked-in.svg";
+import { NavLink } from "react-router-dom";
+import svg from "@/assets/robot.svg";
+import clsx from "clsx";
 
 export function Home() {
   return (
     <div className="relative size-full">
-      <div className="absolute top-0 right-0 flex flex-row items-center gap-4">
-        <img
-          src={github as string}
-          className="h-8 md:h-14 z-20 cursor-pointer hover:scale-105 transition-all duration-300"
-          alt={"Github"}
-        />
-        <div className="h-6 md:h-12 bg-teal-200/30 w-0.5 rounded-full"></div>
-        <img
-          src={linkedIn as string}
-          className="h-8 md:h-14 z-20 cursor-pointer hover:scale-110 transition-all duration-300"
-          alt={"LinkedIn"}
-        />
-      </div>
+      <div className="flex-row flex gap-16 absolute md:px-4 inset-0 justify-center top-[15%]">
+        <div className="flex flex-col max-w-lg gap-16 md:gap-20">
+          <div className="grid gap-6">
+            <div className="text-5xl md:text-7xl font-extrabold text-white leading-tight">
+              <span>Let us take care of your security</span>
+              <span className="text-primary-light"> .</span>
+            </div>
 
-      <div className="size-full p-8 flex flex-row container mx-auto justify-between gap-4">
-        <div className="flex flex-col gap-12 md:gap-16 justify-center text-start">
-          <div className="flex flex-col gap-2">
-            <span className="ms-2 text-4xl sm:text-5xl md:text-6xl lg:text-6xl xl:text-7xl font-light text-zinc-400">
-              Welcome to
-            </span>
-            <span className="text-7xl sm:text-8xl md:text-9xl lg:text-9xl font-extrabold text-teal-200">
-              OATH.
-            </span>
+            <div className="font-montserrat text-wrap text-2xl text-neutral">
+              <span>Add</span>
+              <span className="font-bold text-accent"> unbreakable </span>
+              <span>security to your app. </span>
+              <span>The safety of your users is one click away.</span>
+            </div>
           </div>
 
-          <div className="font-light text-md md:text-2xl max-w-md text-zinc-400">
-            <span> Add </span>
-            <span className="text-pink-500 font-bold leading-10">
-              {" "}
-              unbroken{" "}
-            </span>
-            <span>
-              {" "}
-              security to your app now. The safety of your users is one click
-              away.{" "}
-            </span>
-          </div>
-
-          <button className="bg-teal-200/30 text-teal-100 text-xl rounded-full py-3 px-8 w-fit outline outline-1 hover:outline-2 hover:outline-teal-200 ">
+          <NavLink
+            to="/login"
+            className={clsx(
+              "text-dark font-semibold text-2xl",
+              "w-fit rounded-full py-6 px-10",
+              "bg-primary hover:bg-primary-light",
+              "md:py-4 md:text-xl text-center"
+            )}
+          >
             Get started
-          </button>
+          </NavLink>
         </div>
-        <div className="group relative hidden lg:flex lg:flex-col justify-center 2xl:w-1/2">
-          <img src={svg as string} className="z-20" alt={"Laptop"} />
-          <div className="absolute top-50 left-0 w-full aspect-square border-2 border-teal-200 bg-teal-200/30 rounded-full -z-10 transition-all duration-300 group-hover:shadow-2xl group-hover:shadow-pink-400"></div>
+
+        <div className="group relative hidden lg:block place-items-center">
+          <img src={svg as string} className="z-20 w-2/3" alt={"Robot"} />
+          <div
+            className={clsx(
+              "transition-all duration-300",
+              "bg-primary w-2/3 aspect-square rounded-full",
+              "absolute -z-10 top-[10%] left-1/2 -translate-x-1/2",
+              "group-hover:shadow-2xl group-hover:shadow-primary-light"
+            )}
+          />
         </div>
       </div>
     </div>
