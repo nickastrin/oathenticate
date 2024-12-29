765022aa2640aa2b0e3ebe42aafa0e282c279d18
nastrinakis <nastrinakis@softone.gr>
2024-12-29 13:41:47 +0200

    Fix input registration bug

--- auth-frontend/src/components/forms/FormPasswordInput.tsx
+++ auth-frontend/src/components/forms/FormPasswordInput.tsx
@@ -19,7 +19,6 @@ export function FormPasswordInput({
 
   return (
     <div
-      {...register(name)}
       className={clsx(
         "form-input text-neutral bg-dark w-full",
         "flex flex-row place-items-center gap-3 p-3",
@@ -29,9 +28,10 @@ export function FormPasswordInput({
       <span className="material-symbols-rounded">{icon}</span>
 
       <input
+        {...register(name)}
         type={isPasswordHidden ? "password" : "text"}
         className={clsx(
-          "appearance-none focus:outline-none",
+          "appearance-none focus:outline-none grow",
           "bg-transparent placeholder:text-neutral-dark pe-1"
         )}
         placeholder={placeholder}
