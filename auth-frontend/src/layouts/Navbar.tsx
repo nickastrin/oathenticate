937b62973ce86c115d0e2459cfda578f0a5d58d5
Nikos Astrinakis <nikos.astrin@gmail.com>
2025-01-06 13:11:01 +0200

    Add expanded sidebar toggling

--- auth-frontend/src/layouts/Navbar.tsx
+++ auth-frontend/src/layouts/Navbar.tsx
@@ -1,19 +1,34 @@
 import { Logo } from "@/components/generics/generics";
+import { useState } from "react";
+import { ExpandedSidebar } from "./ExpandedSidebar";
 import clsx from "clsx";
 
 export function Navbar() {
-  return (
-    <div
-      className={clsx(
-        "flex flex-row place-items-center",
-        "w-full py-4 px-6 bg-darker gap-2"
-      )}
-    >
-      <button className="p-0 bg-transparent flex place-items-center">
-        <span className="material-symbols-rounded"> menu </span>
-      </button>
+  const [showSidebar, setShowSidebar] = useState(false);
 
-      <Logo />
-    </div>
+  return (
+    <>
+      <div
+        className={clsx(
+          "flex flex-row place-items-center",
+          "w-full py-4 px-6 bg-darker gap-2",
+        )}
+      >
+        <button
+          className="p-0 bg-transparent flex place-items-center"
+          onClick={() => setShowSidebar(!showSidebar)}
+        >
+          <span className="material-symbols-rounded"> menu </span>
+        </button>
+
+        <Logo />
+      </div>
+
+      <ExpandedSidebar
+        show={showSidebar}
+        onClose={() => setShowSidebar(false)}
+        withBackdrop
+      />
+    </>
   );
 }
